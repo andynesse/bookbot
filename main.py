eeadc92f543aed3f433f63469da31d5d1bd40483
andynesse <andynesse04@gmail.com>
2025-07-03 20:51:29 +0200

    finished: nice printing and choose file from cmd args

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from stats import word_count, character_count
+import sys
+from stats import word_count, character_count, sort_character_counts
 
 def get_book_text(file):
     with open(file, "r", encoding="utf-8") as f:
         return f.read()
 
 def main():
-    file = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file = sys.argv[1]
     word_c = word_count(get_book_text(file))
-    chars_c = character_count(get_book_text(file))
-    print(word_c, "words found in the document")
-    print(chars_c)
+    chars_c = sort_character_counts(character_count(get_book_text(file)))
+    print(f"""
+          ============ BOOKBOT ============\n
+          Analyzing book found at {file}...\n
+          ----------- Word Count ----------\n
+          Found {word_c} total words\n
+          --------- Character Count -------
+          """)
+    for char in chars_c:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,14 @@ def character_count(txt):
             chars_counts[char] = 0
         chars_counts[char] += 1
     return chars_counts
+
+def sort_character_counts(char_counts):
+    char_c_list = []
+    for key in char_counts.keys():
+        char_c_list.append({
+            "char": key,
+            "num": char_counts[key]
+        })
+    char_c_list.sort(reverse=True, key=lambda items: items["num"])
+    return char_c_list
+    
